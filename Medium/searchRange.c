e3b8c8c78ba70fa1e8bbb314c23662e730f1fdd2
Alexandre Boavida <a68781@ualg.pt>
2022-06-30 20:35:23 +0100

    Made code more compact

--- Medium/searchRange.c
+++ Medium/searchRange.c
@@ -17,32 +17,23 @@ Memory Usage: 7.6  MB
 int binarysearch(int *nums, int n, int target, bool go_right)
 {
     int low = 0, high = n - 1;
- 
     int result = -1;
- 
-    while (low <= high)
-    {
+    while (low <= high){
         int mid = (low + high)/2;
- 
-      
-        if (target == nums[mid])
-        {
+        if (target == nums[mid]){
             result = mid;
             if(go_right)
                 low = mid+1;
             else
                 high = mid - 1;
         }
- 
         else if (target < nums[mid]) {
             high = mid - 1;
         }
- 
         else {
             low = mid + 1;
         }
     }
- 
     return result;
 }
 
