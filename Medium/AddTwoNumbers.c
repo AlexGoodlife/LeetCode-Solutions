9e877afdd66e593dda6acd5490a429a7216e92d8
Alexandre Boavida <a68781@ualg.pt>
2022-06-30 20:32:42 +0100

    Changed some syntax

--- Medium/AddTwoNumbers.c
+++ Medium/AddTwoNumbers.c
@@ -18,7 +18,7 @@ struct ListNode {
 };
 
 
-struct ListNode* cons(int x, struct ListNode* s)
+struct ListNode* append(int x, struct ListNode* s)
 {
   struct ListNode * r = (struct ListNode*) malloc(sizeof(struct ListNode));
   r -> val = x;
@@ -49,14 +49,14 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
         int second = (l2 != NULL) ? l2->val : 0;
         put = first + second + carry;
         carry = put / 10;
-        r = cons(put % 10, r);
+        r =append(put % 10, r);
         if(l1 != NULL)
             l1 = l1->next;
         if(l2 != NULL)
             l2 = l2->next;
     }
     if(carry > 0){
-        r = cons(carry, r);
+        r =append(carry, r);
     }
     struct listNode * result = reverseList(r);
     return result;
